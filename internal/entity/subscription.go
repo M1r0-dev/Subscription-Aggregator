a3cdfb5016f0fdfb74f49e08599ca9d965e365a7
Mikhailin Artem <m1r0tvorxc.dev@gmail.com>
2025-10-15 16:50:11 +0300

    feat[entity]: implement subscription structure

---
+++ internal/entity/subscription.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Subscription struct {
+    Id          int64     `db:"id" json:"id"`
+    ServiceName string    `db:"service_name" json:"service_name"`
+    Price       uint64    `db:"price" json:"price"`
+    UserID      uuid.UUID `db:"user_id" json:"user_id"`
+    StartDate   time.Time `db:"start_date" json:"start_date"`
+	EndDate  time.Time `db:"end_date" json:"end_date"`
+}
+
