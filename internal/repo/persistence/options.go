003c92025dd29cc7d48d666990714f4d5556d372
Mikhailin Artem <m1r0tvorxc.dev@gmail.com>
2025-10-16 16:25:01 +0300

    feat[repo]: add options to filter

--- internal/repo/persistence/options.go
+++ internal/repo/persistence/options.go
@@ -19,3 +19,33 @@ type ListOptions struct {
 	SortBy        string
 	SortOrder     string
 }
+
+func WithUserID(id uuid.UUID) ListOption {
+	return func(l *ListOptions) {
+		l.UserID = &id
+	}
+}
+
+func WithServiceName(name string) ListOption {
+	return func(l *ListOptions) {
+		l.ServiceName = &name
+	}
+}
+
+func WithPrice(price uint64) ListOption {
+	return func(l *ListOptions) {
+		l.Price = &price
+	}
+}
+
+func WithStartDateFrom(t time.Time) ListOption {
+	return func(l *ListOptions) {
+		l.StartDateFrom = &t
+	}
+}
+
+func WithStartDateTo(t time.Time) ListOption {
+	return func(l *ListOptions) {
+		l.StartDateTo = &t
+	}
+}
